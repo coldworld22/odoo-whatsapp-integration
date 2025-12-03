82487f25d10c881b66d472b79ef23e58b1ed1c21
root <root@srv897288.hstgr.cloud>
2025-12-03 05:58:25 +0000

    Update skillbridge login

--- custom-addons/skillbridge_login/__manifest__.py
+++ custom-addons/skillbridge_login/__manifest__.py
@@ -1,12 +1,12 @@
 {
     'name': 'SkillBridge Login Page',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'SkillBridge',
     'category': 'Theme',
     'website': 'https://eduskillbridge.net',
     'summary': 'Custom corporate login page for SkillBridge',
     'description': 'Professional customized login page with SkillBridge branding.',
-    'depends': ['web'],
+    'depends': ['web', 'auth_oauth'],
     'data': [
         'views/login_template.xml',
     ],
